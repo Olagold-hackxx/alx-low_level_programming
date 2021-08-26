07c403db3af51727c625fd9a0a7ffc48887dd202
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-08-26 08:52:29 -0700

    C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - prints size of types
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %zu byte(s)", sizeof(a));
+	printf("Size of an int: %zu byte(s)", sizeof(b));
+	printf("Size of a long int: %zu byte(s)", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)", sizeof(d));
+	printf("Size of a float: %zu byte(s)", sizeof(e));
+	return (0);
+}
