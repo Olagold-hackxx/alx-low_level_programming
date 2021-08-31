526c560f8496384a037a39e5cfd266279ef50cb2
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-08-31 13:48:00 -0700

    C functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * @c: lowercas char to print
  * Return: Always 0
  */
-int _islower(int c);
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#inclide "main.h"
+#include "main.h"
 /**
  * _isalpha - prints alpha
  * @c: alpha to print
