e410c71664f357275a9e3ee322db97d887eff3c9
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-08-31 14:03:41 -0700

    C functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - print signs of char
+ * @n: char whose signs are printed
+ * Return: 1 on success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		return (-1);
+	}
+}
