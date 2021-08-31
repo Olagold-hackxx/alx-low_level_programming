f155a179f4f754ec34c4c321573e5bd40795b940
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-08-31 13:43:32 -0700

    C functions

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#inclide "main.h"
+/**
+ * _isalpha - prints alpha
+ * @c: alpha to print
+ * Return: 1 on success
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
