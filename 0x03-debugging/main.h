72763342e69ea2872de6371951c038b28f87ef3f
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-09-01 16:35:47 -0700

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 void positive_or_negative(int i);
-int largest_number(int, int, int);
-int convert_day(int, int);
-void print_remaining_days(int a, int b, int c);
+int largest_number(int a, int b, int c);
+
+#endif
