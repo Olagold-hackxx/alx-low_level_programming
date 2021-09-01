2613629952e28c4acdcd327ddc12f1f74b7da939
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-08-31 18:41:23 -0700

    C functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,23 +5,26 @@
  */
 void times_table(void)
 {
-	int i, k;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (k = k * i; k < 82; k++)
+		for (j = 0; j < 10; j++)
 		{
-			if (k < 10)
+			for (k = i * j; k < 82; k++)
 			{
-				_putchar(k);
+				if (k < 10)
+				{
+					_putchar(k + '0');
+				}
+				else
+				{
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				_putchar(',');
+				_putchar(' ');
 			}
-			else
-			{
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
