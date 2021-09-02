656ec396a17d8b8831cfe7b737f4fc7e02ea3e86
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-09-02 11:42:43 -0700

    Nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply integers
+ * @a: int to multiply
+ * @b: int to multiply
+ * Return: multiply
+ */
+
+int mul(int a, int b)
+{
+	multiply = a * b;
+
+	return (multiply);
+}
