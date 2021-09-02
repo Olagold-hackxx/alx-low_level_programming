985ea8f93178880418a1724bde24a5c59612a1ad
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-09-02 08:57:07 -0700

    C functions

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,9 +13,16 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
 	{
-		if (month > 2 && day > 59)
+		if (month >= 2 && day >= 60 )
 		{
-			day++;
+			if (month = 2 && day = 60)
+			{
+				day = 60;
+			}
+			else
+			{
+				day++;
+			}
 		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
