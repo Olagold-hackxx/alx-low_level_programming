12caad00bfd4c9b87e60e9b16974a7f79fefffa2
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-09-02 12:33:23 -0700

    Nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
@@ -10,5 +5,3 @@ void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
-
-#endif
