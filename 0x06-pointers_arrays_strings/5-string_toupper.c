55d696494c918b2981c3e9833aa2b16567cf7e6d
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2021-09-13 16:34:09 +0000

    Arrays Pointers and String

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * string_toupper - change string to upper case
+ * @s: pointer to string
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}
