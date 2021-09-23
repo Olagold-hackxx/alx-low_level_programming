62fb72cc5c20b9576b19999c2417d692e4c2e9cf
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-09-23 13:46:11 -0700

    more malloc and free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates bytes of s2 to s1
+ * @s1: string 1
+ * @s2: string 2
+ * @n: no of bytes of s2 to be concatenated
+ * Return: concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i, j, len = _strlen(s1);
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	str = malloc((len + n + 1) * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		str[i] = s1[i];
+	for (j = 0; j < n; j++)
+	{
+		str[i + j] = s2[j];
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+/**
+ * _strlen - gets string length
+ * @s: pointer to string
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen(s + 1));
+}
