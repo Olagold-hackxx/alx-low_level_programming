43e1d89dd3d7a4a0a56ede8ab05604df38bf8699
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-09-27 02:43:30 -0700

    calloc and realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,8 +30,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	for (i = 0; i < old_size; i++)
-		alloc[i] = 0;
-	alloc = malloc(new_size);
-	ptr = alloc;
-	return (ptr);
+		(int *)*(ptr + i) = 0;
+	free(ptr);
+	ptr = malloc(new_size);
+	return ((void *)ptr);
 }
