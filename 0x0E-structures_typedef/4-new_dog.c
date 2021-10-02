507b2c90497656864170269e72db5278f9cdd67d
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-10-02 05:52:57 -0700

    Struct and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,6 +24,7 @@ int _strlen(char *s)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0;
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
@@ -31,12 +32,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	dog->name = malloc(sizeof(dog_t) * _strlen(name));
 	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
+	}
 	dog->owner = malloc(sizeof(dog_t) * _strlen(owner));
-	if (dog->name == NULL)
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
 		return (NULL);
-	dog->name = name;
+	}
+	for (i = 0; i <= _strlen(name); i++)
+		dog->name[i] = name[i];
 	dog->age = age;
-	dog->owner = owner;
+	for (i = 0; i <= _strlen(owner); i++)
+		dog->owner[i] = owner[i];
 	return (dog);
 }
