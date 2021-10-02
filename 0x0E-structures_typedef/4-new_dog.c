3bd0bcb65a29499136ff24140fbcfb6f98d5e328
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2021-10-02 05:31:39 -0700

    Struct and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,19 @@
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * _strlen - get string length
+ * @s: pointer to string
+ * Return: string length
+ */
 
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
 /**
  * new_dog - copy and store struct
  * @name: pointer element of struct dog
@@ -27,17 +40,3 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = owner;
 	return (dog);
 }
-
-/**
- * _strlen - get string length
- * @s: pointer to string
- * Return: string length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-	return (len);
