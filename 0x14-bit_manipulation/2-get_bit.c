9e3f4754689158654d7917ea7f2ab1e71ca2e236
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2022-03-03 03:30:46 -0800

    bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - get bit at given index
+ * @n: mumber whose bit we're looking for at given index
+ * @index: given index
+ * Return: the bi at the index or -1 for error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i <= 100; i++)
+	{
+		if (index == i)
+			return (n & 1);
+		n = n >> 1;
+	}
+	return (-1);
+}
+
