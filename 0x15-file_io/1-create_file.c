bc43151f90c02a9b44cb277de96d87adf2097244
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2022-03-05 03:19:43 -0800

    filei i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,6 +4,7 @@
  * create_file - create a file or truncate if exist
  * @filename: name of file
  * @text_content: content to be added or truncated
+ * Return: 1 if succes -1 if failure
  */
 
 int create_file(const char *filename, char *text_content)
@@ -11,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t writeLength;
 
-	fd = open(filename, O_CREAT| O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	writeLength = write(fd, text_content, strlen(text_content) + 1);
