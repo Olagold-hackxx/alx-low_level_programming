ca81d2b8206df1be0011d917ebd9ad9925dff429
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2022-03-07 15:27:53 -0800

    File i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	writeLength = write(fd, text_content, strlen(text_content) + 1);
+	writeLength = write(fd, text_content, strlen(text_content));
 	if (!filename | (writeLength == -1))
 		return (-1);
 	return (1);
