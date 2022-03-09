0f13a5b26f0da7d3eefafe3a6f5e7671418a8259
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2022-03-09 00:09:28 -0800

    file copying~

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,6 +12,7 @@ int main(int ac, char **av)
 {
 	int fd_from, fd_to;
 	char buf[1024];
+	ssize_t writeLength, readLength;
 
 	if (ac != 3)
 	{
@@ -21,18 +22,28 @@ int main(int ac, char **av)
 	fd_from = open(av[0], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[0]);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fd_to = open(av[1], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[1]);
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	while (read(fd_from, buf, 1024))
+	while (readLength = read(fd_from, buf, 1024))
 	{
-		write(fd_to, buf, 1024);
+		writeLength = write(fd_to, buf, 1024);
+	}
+	if (!av[1] | readLength == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (writeLength == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
 	}
 	if (close(fd_from) < 0)
 	{
@@ -46,5 +57,5 @@ int main(int ac, char **av)
 	}
 	close(fd_from);
 	close(fd_to);
-	return (1);
+	return (0);
 }
