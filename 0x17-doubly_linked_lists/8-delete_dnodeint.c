e61914cbebd372e857b1b7584f4c0f3385f43e88
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2022-06-12 14:29:23 -0700

    Fix olde folders errors

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,23 +1,43 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * delete_dnodeint_at_index - del node in doubly linked list node
- * @head: ptr to ptr to head
- * @index: index for node to be del
- * Return: 1 on success -1 on failure
+ * insert_dnodeint_at_index - insert node at an index
+ * @h: ptr to ptr to head
+ * @idx: index to insert node
+ * @n: int to insert at node
+ * Return: adress of new node
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *current, *temp;
 	unsigned int i;
-	dlistint_t *current = *head;
 
-	for (i = 0; i < index; i++)
-		current = current->next;
-	if (current != NULL)
+	current = *head;
+	temp = *head;
+	i = 0;
+	while (current)
 	{
-		current->prev->next = current->next;
-		current->next->prev = current->prev;
-		return (1);
+		if (i != index)
+			current = current->next;
+		else
+		{
+			if (current->prev)
+			{
+				current->prev->next = current->next;
+				current->next->prev = current->prev;
+				free(current);
+				return (1);
+			}
+			else
+			{
+				current = current->next;
+				current->prev = NULL;
+				free(temp);
+				return (1);
+			}
+		}
+		i++;
 	}
 	return (-1);
 }
