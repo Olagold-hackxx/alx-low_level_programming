14a3ae4a8357077fb3474b57800480b607994a84
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2022-06-18 08:42:40 -0700

    fix errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 
-	if (ht == NULL || key == NULL || key == "")
+	if (ht == NULL || *key == NULL || key == "")
 		return (0);
 
 	while (tmp)
