3d9bcdcf338566226c9f06c3d002182ecf8eafc6
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-05-02 15:32:34 -0400

    Advance tasks

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -84,5 +84,7 @@ void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_print(const shash_table_t *ht);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+shash_table_t *shash_table_create(unsigned long int size);
+
 
 #endif /* HASH_TABLES_H */
