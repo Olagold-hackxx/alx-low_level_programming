c6c53ea2ce7580d2125e9c2e2d028bb1677d118c
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-11 21:35:01 -0400

    Search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,7 +5,7 @@ int binary_search_recursive(int *array, int firstIndex, int size, int value);
 
 
 /**
- * binary_search - Implement linear search algorithm
+ * binary_search - Implement binary search algorithm
  * @array: array to search
  * @size: size of array
  * @value: value to search for
@@ -18,24 +18,39 @@ int binary_search(int *array, size_t size, int value)
 	return (binary_search_recursive(array, 0, size - 1, value));
 }
 
+/**
+ * binary_search_recursive - Implement binary search algorithm recursively
+ * @array: array to search
+ * @firstIndex: first index of array
+ * @size: size of array
+ * @value: value to search for
+ * Return: index of value or -1 if not found
+*/
 int binary_search_recursive(int *array, int firstIndex, int size, int value)
 {
 	int mid;
 
-    if (size >= firstIndex)
+	if (size >= firstIndex)
 	{
-        mid = firstIndex + (size - firstIndex) / 2;
+		mid = firstIndex + (size - firstIndex) / 2;
 		printarray(array, size, firstIndex);
-        if (array[mid] == value)
-            return mid;
+		if (array[mid] == value)
+			return (mid);
 
-        if (array[mid] > value)
-            return binary_search_recursive(array, firstIndex, mid - 1, value);
-		return binary_search_recursive(array, mid + 1, size, value);
+		if (array[mid] > value)
+			return (binary_search_recursive(array, firstIndex, mid - 1, value));
+		return (binary_search_recursive(array, mid + 1, size, value));
 	}
 	return (-1);
 }
 
+
+/**
+ * printarray - Print array
+ * @array: array to search
+ * @size: size of array
+ * @start: index to start prnting
+*/
 void printarray(int *array, int size, int start)
 {
 	int i;
